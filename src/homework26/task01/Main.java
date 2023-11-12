9b9ba6f6a33484d1ded004b907f9b7ff68a7c417
Roman Luchenkov <lucencov.roman@gmail.com>
2023-11-12 22:23:44 +0100

    add task01 solution

---
+++ src/homework26/task01/Main.java
@@ -0,0 +1,15 @@
+package homework26.task01;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Human jack = new Human("Jack", 35);
+    Human ellis = new Human("Ellis", 28);
+    Human bob = new Human("Robert", 31);
+
+    jack.introduce();
+    ellis.introduce();
+    bob.introduce();
+  }
+
+}
