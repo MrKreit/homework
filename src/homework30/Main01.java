abcd378045bcc088adf1def89655be4a403b210e
Roman Luchenkov <lucencov.roman@gmail.com>
2023-11-19 22:43:05 +0100

    add main01 solution

---
+++ src/homework30/Main01.java
@@ -0,0 +1,20 @@
+package homework30;
+
+public class Main01 {
+
+  //примитивные типы данных: byte, short, int, long,
+  //float, double.
+  //char
+  //boolean
+  public static void main(String[] args) {
+    int a = 7;
+    double b = 7.7;
+    char R = 'R';
+    boolean c = true;
+
+    System.out.println("Целый: " + a + " 4 байт");
+    System.out.println("Нецелый: " + b + " 8 байт");
+    System.out.println("Символьный: " + R + " 1 байт");
+    System.out.println("Логический: " + c + " 1 байт");
+  }
+}
