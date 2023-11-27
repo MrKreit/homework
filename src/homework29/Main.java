835a2b82ddcf63a36e63e4c467ccc8e12d38b857
Roman Luchenkov <lucencov.roman@gmail.com>
2023-11-27 16:23:27 +0100

    correct homework29

--- src/homework29/Main.java
+++ src/homework29/Main.java
@@ -3,12 +3,10 @@ package homework29;
 public class Main {
 
   public static void main(String[] args) {
-   TraficLight traficLight = new TraficLight("GREEN");
-   traficLight.setColor("GREEN");
+   TraficLight traficLight = new TraficLight(ColorTrafficLight.GREEN);
+  // traficLight.setColor("GREEN");
   // TraficLight.canCrossTheRoad("GREEN");
     System.out.println(TraficLight.canCrossTheRoad("GREEN"));
 
-
   }
-
 }
--- src/homework29/TraficLight.java
+++ src/homework29/TraficLight.java
@@ -3,27 +3,27 @@ package homework29;
 
 public class TraficLight {
 
-  private String currentTrafficLight;
+  private ColorTrafficLight currentTrafficLight;
 
-  public TraficLight(String currentTrafficLight) {
+  public TraficLight(ColorTrafficLight currentTrafficLight) {
     this.currentTrafficLight = currentTrafficLight;
   }
 
-
-  public String getColor() {
+  public ColorTrafficLight getColor() {
     return currentTrafficLight;
   }
 
-  public void setColor(String currentTrafficLight) {
+  public void setColor(ColorTrafficLight currentTrafficLight) {
     this.currentTrafficLight = currentTrafficLight;
   }
-public static boolean canCrossTheRoad(String color ) {
+
+  public static boolean canCrossTheRoad(String color) {
     if (color.equals("GREEN")) {
       return true;
     } else {
       return false;
     }
-}
+  }
 
 }
 
