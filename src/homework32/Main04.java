219511ed8abd84efbf4b5910184c1eba2ee71699
Roman Luchenkov <lucencov.roman@gmail.com>
2023-11-27 01:47:16 +0100

    add task04 solution

--- src/homework32/Main04.java
+++ src/homework32/Main04.java
@@ -5,28 +5,35 @@ import java.util.Scanner;
 public class Main04 {
 
   public static void sort(int[] arr) {
+    boolean sorted = true;
     for (int i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        System.out.println("Array ne otsortirovan");
-      } else {
-        System.out.println("Array otsortirovan");
+        sorted = false;
+        break;
       }
     }
+    if (sorted) {
+      System.out.println("Array  otsortirovan");
+    } else {
 
+      System.out.println("Array nie otsortirovan");
+    }
   }
+
 
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter size array");
     int size = scanner.nextInt();
+    int[] arr = new int[size];
     System.out.println("Enter array");
     for (int i = 0; i < size; i++) {
-      int[] arr = new int[scanner.nextInt()];
+      arr[i] = scanner.nextInt();
+    }
       sort(arr);
 
     }
   }
 
 
-}
