594f909d48041eabce43bae3e541821e1e3f33cc
Roman Luchenkov <lucencov.roman@gmail.com>
2024-01-12 12:38:21 +0100

    add task01 solution

---
+++ src/task01/Pair.java
@@ -0,0 +1,30 @@
+package task01;
+
+import java.util.Arrays;
+
+public class Pair<T, E>{
+
+private T first;
+private E second;
+
+  public Pair(T first, E second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public T getFirst() {
+    return first;
+  }
+
+  public void setFirst(T first) {
+    this.first = first;
+  }
+
+  public E getSecond() {
+    return second;
+  }
+
+  public void setSecond(E second) {
+    this.second = second;
+  }
+}
